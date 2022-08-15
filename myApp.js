711d65692b407f2e4d605c2d5e6ce21a261c3750
Realgreat01 <samsonrealgreat@gmail.com>
2022-08-15 14:25:39 +0100

    first commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+const mongoose = require("mongoose");
+
+mongoose.connect = (process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, ()=>{
+    console.log("connected to database succesfully")
+})
 
 let Person;
 
